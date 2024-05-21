4acf5c52d3988f71a3d086511e49d35856a15c0f
Chris Fall <chrisfall@Chriss-MacBook-Pro.local>
2024-05-21 10:24:19 -0400

    add readme

---
+++ README.md
@@ -0,0 +1,28 @@
+## Introduction
+
+This application is a simple To-Do list which serves as a POC for integrating MongoDB/GraphQL in an application built with Node.js and React.
+
+## Getting Started
+
+Before proceeding to the next steps, ensure you have MongoDB configured in your local enironment at port 27017. 
+
+For further reference: https://www.prisma.io/dataguide/mongodb/setting-up-a-local-mongodb-database
+
+### Install dependencies
+
+`yarn install`
+
+### Run the Application Locally
+
+Back-End
+
+`yarn server`
+
+
+Front End
+
+`yarn client`
+
+## Unit Testing
+
+Unit tests can be run using `yarn test`
--- ui/test/mocks.js
+++ ui/test/mocks.js
@@ -1,26 +1,24 @@
-const { ObjectId } = require('mongoose').Types;
-
 const mockTodos = [
   {
-    "id" : ObjectId("5f6ce226fcc2d67a3cd2aba2"),
+    "id" : "5f6ce226fcc2d67a3cd2aba2",
     "isComplete" : true,
     "isDeleted" : false,
     "text" : "Test Todo"
   },
   {
-    "id" : ObjectId("5f6ce26efcc2d67a3cd2aba3"),
+    "id" : "5f6ce26efcc2d67a3cd2aba3",
     "isComplete" : false,
     "isDeleted" : false,
     "text" : "Test Todo 2"
   },
   {
-    "id" : ObjectId("5f6cf87f037cafc3cc813fde"),
+    "id" : "5f6cf87f037cafc3cc813fde",
     "isComplete" : false,
     "isDeleted" : true,
     "text" : "Delete Me"
   },
   {
-    "id" : ObjectId("5f74c77b21ab55c9c0ea176b"),
+    "id" : "5f74c77b21ab55c9c0ea176b",
     "isComplete" : true,
     "isDeleted" : true,
     "text" : "Test todo input"
@@ -29,13 +27,13 @@ const mockTodos = [
 
 const nonDeletedTodos = [
   {
-    "id" : ObjectId("5f6ce226fcc2d67a3cd2aba2"),
+    "id" : "5f6ce226fcc2d67a3cd2aba2",
     "isComplete" : true,
     "isDeleted" : false,
     "text" : "Test Todo"
   },
   {
-    "id" : ObjectId("5f6ce26efcc2d67a3cd2aba3"),
+    "id" : "5f6ce26efcc2d67a3cd2aba3",
     "isComplete" : false,
     "isDeleted" : false,
     "text" : "Test Todo 2"
@@ -44,13 +42,13 @@ const nonDeletedTodos = [
 
 const deletedTodos = [
   {
-    "id" : ObjectId("5f6cf87f037cafc3cc813fde"),
+    "id" : "5f6cf87f037cafc3cc813fde",
     "isComplete" : false,
     "isDeleted" : true,
     "text" : "Delete Me"
   },
   {
-    "id" : ObjectId("5f74c77b21ab55c9c0ea176b"),
+    "id" : "5f74c77b21ab55c9c0ea176b",
     "isComplete" : true,
     "isDeleted" : true,
     "text" : "Test todo input"
